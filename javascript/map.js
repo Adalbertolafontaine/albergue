392e50e242c23778ec529144ed98c3706ec985b8
adalbertolafontaine <lolo503@hotmail.com>
2022-09-20 11:15:13 -0400

    Ajuste a seleccion de barrios o parajes

--- javascript/map.js
+++ javascript/map.js
@@ -46,8 +46,6 @@ async function Llamar(dirrecion, datos, tipo, formulario) {
       return data;
     })
     .catch((err) => {
-      //console.log(err);
-
       return [];
     });
 }
@@ -66,7 +64,7 @@ let validarLocalidad = (original, comparacion) => {
 
 let alertasdb = await Llamar("https://sdo.do/sibe/alertas");
 let alertas = {};
-console.log(alertasdb);
+
 alertasdb["data"].map((x) => {
   alertas[x["prov"]] = x["alerta"];
 });
@@ -189,6 +187,14 @@ let mapa = {
   features: [],
 };
 
+
+let mapa_barrio = {
+  type: "FeatureCollection",
+  name: "División Politica",
+  crs: { type: "name", properties: { name: "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+  features: [],
+};
+
 let casosactivos = datos.activos.map((c) => {
   return L.marker([c["Y"], c["x"]]).bindPopup(
     `${c["nombre"]} <br> Ocupación ${c.cantidad || 0}`
@@ -205,14 +211,17 @@ let municipio_geo = L.geoJson(mapa, {
   filter: filtro,
 }).addTo(map);
 
+let barrios_geo = L.geoJson(mapa_barrio, {
+  style: style,
+  onEachFeature: onEachFeature,
+  filter: filtro,
+}).addTo(map);
 
 /* global statesData */
 geojson = L.geoJson(provincias, {
   style: style,
   onEachFeature: onEachFeature,
 }).addTo(map);
-
-
 
 let casos_activos = L.layerGroup(casosactivos).addTo(map);
 
@@ -244,23 +253,25 @@ SLprovincia.addEventListener("change", async (e) => {
     let corner1 = L.latLng(borde["max"][1], borde["max"][0]);
     let corner2 = L.latLng(borde["min"][1], borde["min"][0]);
     let bounds = L.latLngBounds(corner1, corner2);
-    let p = await Llamar(`https://sdo.do/sibe/municipio?prov=${SLprovincia.value}`)
-    
-    mapa.features = p['data']
-    
-     console.log(mapa)
-     municipio_geo = L.geoJson(mapa, {
+    let p = await Llamar(
+      `https://sdo.do/sibe/municipio?prov=${SLprovincia.value}`
+    );
+
+    mapa.features = p["data"];
+
+    municipio_geo = L.geoJson(mapa, {
       style: style,
       onEachFeature: onEachFeature,
       filter: filtro,
     }).addTo(map);
 
-
-
     //let p = L.control.layers;
-
-    map.fitBounds([bounds]);
+    let esquina = municipio_geo.getBounds();
+    map.fitBounds(esquina);
   } else {
+
+    let esquina = geojson.getBounds();
+    map.fitBounds(esquina);
     map.addLayer(geojson);
   }
 
@@ -272,6 +283,45 @@ SLprovincia.addEventListener("change", async (e) => {
 
   LLenadoSl(m, "Slmunicipio");
   LLenadoSl([], "Slparaje");
+});
+
+Slmunicipio.addEventListener("change", async (e) => {
+  llenado_cuadro(SLprovincia.value, Slmunicipio.value, "00");
+
+  map.removeLayer(geojson);
+  map.removeLayer(municipio_geo);
+  map.removeLayer(barrios_geo);
+
+  if (Slmunicipio.value !== "00") {
+  
+    
+    let p = await Llamar(
+      `https://sdo.do/sibe/parajes?prov=${SLprovincia.value}&mun=${Slmunicipio.value}`
+    );
+
+    mapa_barrio.features = p["data"];
+
+    barrios_geo = L.geoJson(mapa_barrio, {
+      style: style,
+      onEachFeature: onEachFeature,
+      filter: filtro,
+    }).addTo(map);
+
+    //let p = L.control.layers;
+    let t = barrios_geo.getBounds();
+    map.fitBounds(t);
+  } else {
+    map.addLayer(municipio_geo);
+  }
+
+  let m = municipio
+    .filter((x) => x.prov === SLprovincia.value)
+    .map((x) => {
+      return { id: x.mun, descripcion: x.nombre };
+    });
+
+  //  LLenadoSl(m, "Slmunicipio");
+  // LLenadoSl([], "Slparaje");
 });
 
 const CantidadAlbergue = (y) => {
